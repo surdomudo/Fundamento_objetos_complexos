8dadb9b8a5b4aa61c0ad2e139d166b0b3d37e59b
surdomudo <140193686+surdomudo@users.noreply.github.com>
2024-06-11 19:13:44 +0000

    adicionando array

---
+++ array.js
@@ -0,0 +1,24 @@
+// const nomes = ["Bruno", "João", "Maria"] // Array 
+// nomes[3] = "Paulo" // Adiciona um novo valor ao array no indice informado
+// nomes[2] = "Joana" // Atualiza o valor no array no indice informada
+// console.log(nomes[2])
+// console.log(nomes.length) // Através do length é um atributo do array que informa o tamanho dele
+// nomes.push("Tulio") // ADICIONA UM ELEMENTO NO FINAL DO ARRAY 
+// const ultimoNome =  nomes.pop() //REMOVE O ULTIMO ELEMENTO DO ARRAY E RETORTNA
+
+const comidasFavoritas = []
+
+//LANCHES:
+comidasFavoritas.push("pastel")
+comidasFavoritas.push("coxinha")
+comidasFavoritas.push("pizza")
+
+//FRUTAS:
+comidasFavoritas.push("manga")
+comidasFavoritas.push("maça")
+comidasFavoritas.push("morango")
+
+function ordenarComidasFavoritas(comidasFavoritas){
+    const comidasOrdenadas = 
+}
+
